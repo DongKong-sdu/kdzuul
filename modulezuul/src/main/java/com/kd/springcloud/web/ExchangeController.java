db60848f8afd31fc36e195044e6294b3d846c031
lsw <1416229854@qq.com>
2022-03-20 14:43:21 +0800

    lsw first commit

--- modulezuul/src/main/java/com/kd/springcloud/web/ExchangeController.java
+++ modulezuul/src/main/java/com/kd/springcloud/web/ExchangeController.java
@@ -3,6 +3,7 @@ package com.kd.springcloud.web;
 import com.kd.springcloud.entity.Gateway;
 import com.kd.springcloud.params.GatewayParam;
 import com.kd.springcloud.params.GatewayUParam;
+import com.kd.springcloud.params.ZuulParam;
 import com.kd.springcloud.result.JsonResult;
 import com.kd.springcloud.service.ExchangeService;
 import com.kd.springcloud.service.impl.ExchangeServiceImpl;
@@ -16,9 +17,9 @@ import java.util.List;
 public class ExchangeController {
     @Autowired
     private ExchangeServiceImpl exchangeServiceImpl;
-    @RequestMapping("/update/{serviceid}/{cloudserviceid}")
-    public JsonResult updateGateway(@PathVariable String serviceid,@PathVariable String cloudserviceid){
-        return exchangeServiceImpl.updateGateway(serviceid,cloudserviceid);
+    @PostMapping("/updateZuul")
+    public JsonResult updateGateway(@RequestBody ZuulParam zuulParam){
+        return exchangeServiceImpl.updateGateway(zuulParam.getServiceId(),zuulParam.getCloudServiceId());
     }
     @GetMapping("/getList")
     public JsonResult getList(){
